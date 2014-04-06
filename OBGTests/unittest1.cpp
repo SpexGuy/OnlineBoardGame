9309ff6f2bddcc231474eba71771b9422488db3d
Martin Wickham <spexguy070@gmail.com>
2014-04-06 14:03:43 -0500

    updated the readme for the test project

---
+++ OBGTests/unittest1.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace OBGTests {
+	TEST_CLASS(SocketTest) {
+	public:
+		TEST_METHOD(TestTCPSocket) {
+			
+		}
+	};
+}
